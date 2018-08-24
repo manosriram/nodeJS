3a6fef2b5c52f0850fa8bc15b336f99d275fe3fe
Mano Sriram <mano.sriram0@gmail.com>
2018-08-25 00:30:54 +0530

    added 01basic

---
+++ JS/01Basics/celsius.js
@@ -0,0 +1,8 @@
+var score = 100;
+score = 150;
+
+const bonus = 50;
+
+var finalScore = (score + bonus) * 1.6;
+
+console.log(finalScore);
---
+++ JS/01Basics/variables.js
@@ -0,0 +1,8 @@
+let name1 = "mano";
+let name2 = "sriram";
+
+let num1 = 10.01;
+let num2 = 12
+
+console.log(name1 + " " + name2);
+console.log(num1+num2);
