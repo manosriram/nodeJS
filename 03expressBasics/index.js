8210e7300514d355df833ed937b4fca2958b45dd
Mano Sriram <mano.sriram0@gmail.com>
2018-09-16 01:49:55 +0530

    added express files more and installed NPM

--- 03expressBasics/index.js
+++ 03expressBasics/index.js
@@ -6,7 +6,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About Page...Welcome!!</h1>');
+    //res.status(200).json({ user: 'Hitesh', age: 20, id: '1234ght567' });
+    res.status(500).json({ error: 'Something Went Wrong...' });
 });
 
 // Route for Contact us and services..
@@ -15,10 +16,27 @@ app.get('/contact', (req, res) => {
     res.send('<h3>Contact us at 1234567890</h3>');
 });
 
-app.get('/services', (req, res) => {
+app.get('/services*of', (req, res) => {
     res.send('Services : <ul><li>NodeJS</li><li>JavaScript</li><li>Express</li></ul>');
 });
 
+app.post('/login', (req, res) => {
+    res.send('login success..');
+})
+
+app.delete('/delete', (req, res) => {
+    res.send('deleted...');
+})
+
+
+app.get("/flights/:from-:to", (req, res) => {
+    res.send(req.params);
+});
+
+
+app.get('/user/:id/status/:statusID', (req, res) => {
+    res.send(req.params);
+})
 
 app.listen(5000, () => {
     console.log('Server Running at Port http://127.0.0.1:5000/');
--- 03expressBasics/test.js
+++ 03expressBasics/test.js
@@ -2,12 +2,29 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+const writeStream = fs.createWriteStream(__dirname + '/writeup.txt');
+const readStream = fs.createReadStream(__dirname + '/readMe.txt');
+readStream.setEncoding('utf-8');
 
+
+// Writing to another File..Through Writable Stream..
+readStream.on('data', (chunk) => {
+    console.log("Chunk Data Received!!");
+    console.log(chunk);
+    writeStream.write(chunk);
+    console.log("Chunk Received at the other end...");
+
+});
+
+
+// Pushing Data Chunk to the Server ..
 app.get('/', (req, res) => {
-    var readStream = fs.createReadStream(__dirname + '/readMe.txt');
+    const readStream = fs.createReadStream('readMe.txt');
     readStream.pipe(res);
 });
 
+// Starting Server...
 app.listen(5000, () => {
-    console.log('Server Running!!');
+    console.log("Listening at PORT 5000");
+
 });
