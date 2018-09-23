4f0e3660338a2de7d87976e6da2a12409d79e447
Mano Sriram <mano.sriram0@gmail.com>
2018-09-23 15:35:11 +0530

    Added routes

--- 09bigStack/index.js
+++ 09bigStack/index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const port = 9000;
+
 const app = express();
 
-const port = process.env.PORT || 3000;
+//bring all routes..
+const auth = require("./routes/api/auth");
+
+//Middleware for Express...
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json);
+
+//mongoDB configuration
+const db = require("./setup/myurl").mongoURL;
+
+//Attempt to connect to database
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected Successfully..."))
+  .catch(error => console.log(error));
 
 app.get("/", (req, res) => {
   res.send("Server Up and Running!!!");
 });
+
+//actual routes
+app.use("/api/auth", auth);
 
 app.listen(port, () => {
   console.log(`Serving Running at ${port}`);
---
+++ 09bigStack/setup/myurl.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURL: "mongodb://mongonode:mano1234@ds113003.mlab.com:13003/mongonode",
+  secret: "mystrongsecret"
+};
