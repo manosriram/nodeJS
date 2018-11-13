2587496de50c819a98c7148add8cdf56b03be8b0
Mano Sriram <mano.sriram0@gmail.com>
2018-11-13 12:05:18 +0530

    tried adding mongodb -- failed

--- 10overflow/index.js
+++ 10overflow/index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 27017;
 const session = require("express-session");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
@@ -18,6 +18,8 @@ const homePage = require("./routes/api/homepage");
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
+
+mongoose.connect();
 
 app.use(
   session({
